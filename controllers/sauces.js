f7c38f46f881275ae2532ea481459abf6a127f9e
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-03-31 16:18:52 +0800

    refactor controller updateSauce avec if/else

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -2,12 +2,14 @@ const sauces = require('../schemas/sauces')
 const fs = require('fs')
 
 exports.listeSauces = (req, res, next) => {
+
     sauces.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(() => res.status(404).json({message: "impossible de recuperer les sauces"}))
 }
 
 exports.createSauce = (req, res, next) => {
+
     const sauceOject = JSON.parse(req.body.sauce);
     const sauce = new sauces({
         ...sauceOject,
@@ -17,6 +19,7 @@ exports.createSauce = (req, res, next) => {
         usersLiked: [],
         usersDisliked: []
     });
+
     sauce.save()
     .then((sauce) => res.status(201).json({sauce}))
     .catch(() => res.status(401).json({message: "La sauce n'a pas été créer"}))
@@ -24,20 +27,27 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.findSauce = (req, res, next) => {
+
     sauces.findOne({_id: req.params.id})
     .then((sauce) => res.status(200).json(sauce))
     .catch(() => res.status(401).json({message: "Cette sauce n'existe pas"}))
 }
 
 exports.updateSauce = (req, res, next) => {
-    const sauceObject = req.file ?
-    {
-        ...JSON.parse(req.body.sauce),
-        imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-    } :
-    { 
-        ...req.body
+    
+    var sauceObject
+    if(req.file === undefined) {
+        sauceObject =  { 
+            ...req.body
+        }
+    } else {
+        console,log(req.file.filename)
+        sauceObject = {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
+        }
     }
+   
     sauces.updateOne({_id: req.params.id}, {...sauceObject, _id: req.params.id}) /* quand il fait la modif de la sauce je confirme l'id */
      .then((sauce) => res.status(200).json({sauce}))
      .catch(() => res.status(400).json({message: "modification impossible"}))
