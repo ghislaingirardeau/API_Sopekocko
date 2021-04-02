7916a27179f76f30e8b3b2f61432970edcd5f212
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-04-02 17:58:51 +0800

    refactor compteur

--- controllers/likes.js
+++ controllers/likes.js
@@ -14,22 +14,25 @@ exports.modifyLikes = (req, res, next) => {
         /* Je recupere les valeurs des index userId que je mets comme conditions */
         var indexLiked = sauce.usersLiked.indexOf(like.userId)
         var indexDisliked = sauce.usersDisliked.indexOf(like.userId)
-        
-        
-        if (like.jaime === 1 && indexLiked === -1 && indexDisliked === -1) {
+
+        if (indexLiked != -1 || indexDisliked != -1) {
+            console.log("vous avez deja liker cette sauce")
+            alert("vous avez deja liker cette sauce")
             
-            var compteur = sauce.likes + 1
-            sauce.likes = compteur
+        }
+        
+        if (like.jaime === 1 && indexLiked === -1 && indexDisliked === -1) {    
+
+            sauce.likes += 1
             sauce.usersLiked.push(like.userId)
             sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
-                .catch(() => res.status(401).json({message: "erreur envoie du like"}))         
-        } 
+                .catch(() => res.status(401).json({message: "erreur envoie du like"}))                     
+        }
         
         if (like.jaime === -1 && indexLiked === -1 && indexDisliked === -1) {
             
-            var compteur = sauce.dislikes + 1
-            sauce.dislikes = compteur
+            sauce.dislikes += 1
             sauce.usersDisliked.push(like.userId)
             sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
@@ -37,12 +40,11 @@ exports.modifyLikes = (req, res, next) => {
         } 
 
         if (like.jaime === 0) { 
-                        
-            
+
+
             if (indexLiked != -1) {
                 
-                var compteur = sauce.likes - 1
-                sauce.likes = compteur
+                sauce.likes -= 1
                 sauce.usersLiked.splice(indexLiked, 1) /* je supprime l'userId a son index array correspondant */
                 sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
@@ -51,8 +53,7 @@ exports.modifyLikes = (req, res, next) => {
 
             if (indexDisliked != -1) {
 
-                var compteur = sauce.dislikes - 1
-                sauce.dislikes = compteur
+                sauce.dislikes -= 1
                 sauce.usersDisliked.splice(indexDisliked, 1)
                 sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
@@ -60,11 +61,7 @@ exports.modifyLikes = (req, res, next) => {
             }
 
         } 
-        else {
-            console.log("vous avez deja liker cette sauce")
-        }   
         
-
         /* RAS POUR TEST A SUPPR */
         if (like.jaime === 2) {
             sauce.dislikes = 0
@@ -80,11 +77,11 @@ exports.modifyLikes = (req, res, next) => {
 }
 
 
-/*     */
 
-/* const ajoutLikes = function (nmbre, like, tableau) {
-    var compteur = like + nmbre
-    like = compteur
-    tableau.push(like.userId)
-    res.status(200).json(sauce) 
+
+/* var ajoutLikes = function (likes, tableau, user) {
+    likes += 1
+    tableau.push(user)
+    console.log(likes)
+}
 } */
