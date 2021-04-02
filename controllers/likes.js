491533148fc383ac2bbe8e986a25bedaaaba90d1
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-04-02 16:57:33 +0800

    creation d'une variable pour recuperer l'index array

--- controllers/likes.js
+++ controllers/likes.js
@@ -11,56 +11,70 @@ exports.modifyLikes = (req, res, next) => {
         })
         
         delete like._id
+        /* Je recupere les valeurs des index userId que je mets comme conditions */
+        var indexLiked = sauce.usersLiked.indexOf(like.userId)
+        var indexDisliked = sauce.usersDisliked.indexOf(like.userId)
         
-        if (like.jaime === 1 && sauce.usersLiked.indexOf(like.userId) === -1 && sauce.usersDisliked.indexOf(like.userId) === -1) {
+        
+        if (like.jaime === 1 && indexLiked === -1 && indexDisliked === -1) {
             
             var compteur = sauce.likes + 1
             sauce.likes = compteur
             sauce.usersLiked.push(like.userId)
             sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
-                .catch(() => res.status(401).json({message: "erreur envoie du like"}))
-          
+                .catch(() => res.status(401).json({message: "erreur envoie du like"}))         
         } 
         
-        if (like.jaime === -1 && sauce.usersLiked.indexOf(like.userId) === -1 && sauce.usersDisliked.indexOf(like.userId) === -1) {
+        if (like.jaime === -1 && indexLiked === -1 && indexDisliked === -1) {
             
             var compteur = sauce.dislikes + 1
             sauce.dislikes = compteur
             sauce.usersDisliked.push(like.userId)
             sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
-                .catch(() => res.status(401).json({message: "erreur envoie du like"}))
-          
+                .catch(() => res.status(401).json({message: "erreur envoie du like"}))          
         } 
 
-        if (like.jaime === 0) {
-
-            if (sauce.usersLiked.indexOf(like.userId) != -1) {
+        if (like.jaime === 0) { 
+                        
+            
+            if (indexLiked != -1) {
                 
                 var compteur = sauce.likes - 1
                 sauce.likes = compteur
-                var index = sauce.usersLiked.indexOf(like.userId)
-                sauce.usersLiked.splice(index, 1)
+                sauce.usersLiked.splice(indexLiked, 1) /* je supprime l'userId a son index array correspondant */
                 sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
                 .catch(() => res.status(401).json({message: "erreur envoie du like"}))                
             }
 
-            if (sauce.usersDisliked.indexOf(like.userId) != -1) {
-                sauce.dislikes = 0
-                sauce.usersDisliked = []
+            if (indexDisliked != -1) {
+
+                var compteur = sauce.dislikes - 1
+                sauce.dislikes = compteur
+                sauce.usersDisliked.splice(indexDisliked, 1)
                 sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
-                .catch(() => res.status(401).json({message: "erreur envoie du like"}))
+                .catch(() => res.status(401).json({message: "erreur envoie du like"}))  
             }
 
-        } else {
+        } 
+        else {
             console.log("vous avez deja liker cette sauce")
+        }   
+        
+
+        /* RAS POUR TEST A SUPPR */
+        if (like.jaime === 2) {
+            sauce.dislikes = 0
+            sauce.likes = 0
+            sauce.usersDisliked = []
+            sauce.usersLiked = []
             sauce.save()
                 .then((sauce) => res.status(201).json(sauce))
-                .catch(() => res.status(401).json({message: "erreur envoie du like"}))
-        }                            
+                .catch(() => res.status(401).json({message: "erreur envoie du like"}))  
+        }
     })
     .catch(() => res.status(400).json({message: "Impossible de trouver la sauce"}))
 }
