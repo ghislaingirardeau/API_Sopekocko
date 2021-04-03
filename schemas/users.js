5f4ebb02468e06c1f298518f5f165c3f9ee44ef0
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-04-03 15:26:18 +0800

    ajout du pluging hidden pour cache email user dans la base de donnée

--- schemas/users.js
+++ schemas/users.js
@@ -1,12 +1,13 @@
 const mongoose = require("mongoose")
 const uniqueValidator = require('mongoose-unique-validator')
-
+const mongooseHidden = require('mongoose-hidden')()
 
 const utilisateurSchema = new mongoose.Schema({
-    email: {type: String, require: true, unique: true}, 
+    email: {type: String, require: true, unique: true, hide: true}, 
     password: {type: String, require: true}
 })
 
 utilisateurSchema.plugin(uniqueValidator)
+utilisateurSchema.plugin(mongooseHidden)
 
 module.exports = mongoose.model('utilisateur', utilisateurSchema)
