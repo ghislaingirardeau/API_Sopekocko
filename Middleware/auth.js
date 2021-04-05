fb055a4e208bab95aba6340331c846cd034b548e
Ghislain Girardeau <g.girardeaupro@sfr.fr>
2021-04-05 16:45:21 +0800

    suppression commentaire auth.js

--- Middleware/auth.js
+++ Middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const authentification = (req, res, next) => {
     try {
-        const token = req.headers.authorization.split(' ')[1] /* A DETERMNIER OU RECUP LE TOKEN: header ou requete */
+        const token = req.headers.authorization.split(' ')[1] 
         const decodedToken = jwt.verify(token, `${process.env.TOKEN_USERS}`)
         const tokenUserId = decodedToken.userId
         if (req.body.userId && req.body.userId != tokenUserId) {
